232cbb10042e19897bf570432f0c2e878740146d
jrnfaria <rui_games@hotmail.com>
2015-10-25 18:35:25 +0000

    Video still has allocation problem

--- Road Detection/Road Detection/mainZe.cpp
+++ Road Detection/Road Detection/mainZe.cpp
@@ -62,7 +62,7 @@ Mat fillRoad(Mat& final_mask, Mat image)
 	//imshow("test", image_final);
 
 
-	return image;
+	return image_final;
 }
 
 Mat roadSegmentation(Mat& image_gray)
@@ -77,15 +77,15 @@ Mat roadSegmentation(Mat& image_gray)
 	GaussianBlur(divided, divided, Size(5, 5), 0, 0);
 
 	adaptiveThreshold(divided, thresh, max_BINARY_value, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 1.5);
+	
+	namedWindow("Ithreshi", WINDOW_KEEPRATIO);
+	imshow("Ithreshi", thresh);
+
 	dilate(thresh, thresh, getStructuringElement(MORPH_RECT, Size(2, 2)));
-
-	//namedWindow("Ithreshi", WINDOW_KEEPRATIO);
-	//imshow("Ithreshi", thresh);
-
 	erode(thresh, thresh, getStructuringElement(MORPH_RECT, Size(2, 2)));
 
-	//namedWindow("threshi", WINDOW_KEEPRATIO);
-	//imshow("threshi", thresh);
+	namedWindow("threshi", WINDOW_KEEPRATIO);
+	imshow("threshi", thresh);
 
 	return thresh;
 }
@@ -99,23 +99,11 @@ Mat CannyHough(Mat& image_gray, Mat& thresh)
 	Canny(thresh, thresh, 0.66*mean, 1.33*mean, 3);
 
 
-	vector<Vec4i> lines;
-	HoughLinesP(thresh, lines, 1, CV_PI / 180, 15, 2, 10);
+	
 
 
-	// draw lines
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		Vec4i l = lines[i];
-		double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
-		angle = angle < 0 ? angle + 360 : angle;
-		if ((angle > 20 && angle < 80) || (angle > 100 && angle < 160) || (angle > 200 && angle < 260) || (angle > 280 && angle < 340)) {
-			line(thresh, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 1, CV_AA);
-		}
-	}
-
-	//namedWindow("hough", WINDOW_KEEPRATIO);
-	//imshow("hough", thresh);
+	namedWindow("hough", WINDOW_KEEPRATIO);
+	imshow("hough", thresh);
 
 	return thresh;
 }
@@ -133,19 +121,43 @@ int main(int argc, char* argv[]) {
 
 	if (option == 1)
 	{
-		image = cv::imread("road4.jpg", 1);
+		image = cv::imread("road6.jpg", 1);
 		if (!image.data)
 			return 0;
 
 		// Read input image
 		Mat thresh, final_mask, image_final;
-		final_mask = Mat::zeros(image.size(), CV_8UC3);
+		final_mask = Mat::zeros(image.size(), CV_8UC1);
 
 		//convert grey
 		cvtColor(image, image_gray, CV_BGR2GRAY);
 		thresh = roadSegmentation(image_gray);
-		final_mask = CannyHough(image_gray, thresh);
-		image_final = fillRoad(final_mask, image);
+		thresh = CannyHough(image_gray, thresh);
+
+		vector<Vec4i> lines;
+		HoughLinesP(thresh, lines, 1, CV_PI / 180, 15, 2, 10);
+
+
+		// draw lines
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			Vec4i l = lines[i];
+			double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
+			angle = angle < 0 ? angle + 360 : angle;
+			if ((angle > 20 && angle < 80) || (angle > 100 && angle < 160) || (angle > 200 && angle < 260) || (angle > 280 && angle < 340)) {
+				line(thresh, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 1, CV_AA);
+			}
+		}
+
+		lines.clear();
+
+		image_final = fillRoad(thresh, image);
+
+		namedWindow("start", WINDOW_KEEPRATIO);
+		imshow("start", image);
+
+		namedWindow("image", WINDOW_AUTOSIZE);
+		imshow("image", image_final);
 
 		waitKey(0);
 
@@ -161,143 +173,156 @@ int main(int argc, char* argv[]) {
 			return 1;
 		}
 
-
+		namedWindow("video", WINDOW_KEEPRATIO);
+		namedWindow("mask", WINDOW_KEEPRATIO);
 		while (video.isOpened())
 		{
 			bool success = video.read(image);
 
-			final_mask = Mat::zeros(image.size(), CV_8UC3);
+			
+			if (success) {
+				final_mask = Mat::zeros(image.size(), CV_8UC3);
 
-			//convert grey
-			cvtColor(image, image_gray, CV_BGR2GRAY);
-			thresh = roadSegmentation(image_gray);
-			final_mask = CannyHough(image_gray, thresh);
-			image_final = fillRoad(final_mask, image);
+				//convert grey
+				cvtColor(image, image_gray, CV_BGR2GRAY);
+				thresh = roadSegmentation(image_gray);
+				thresh = CannyHough(image_gray, thresh);
+				image_final = fillRoad(thresh, image);
+
+				imshow("mask", thresh);
+				imshow("video", image_final);
+			}
+
+			if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+			{
+				cout << "esc key is pressed by user" << endl;
+				break;
+			}
 		}
 	}
 
 }
 
 /*int main(int argc, char* argv[]) {
-	// Read input image
-	Mat image, image_gray, mask, divided, thresh, finalMask,image_final,blue;
-	image = cv::imread("road5.jpg", 1);
-	if (!image.data)
-		return 0;
+// Read input image
+Mat image, image_gray, mask, divided, thresh, finalMask,image_final,blue;
+image = cv::imread("road5.jpg", 1);
+if (!image.data)
+return 0;
 
-	blue= Mat::zeros(image.size(), CV_8UC3);
-	blue.setTo(cv::Scalar(0, 0, 255));
+blue= Mat::zeros(image.size(), CV_8UC3);
+blue.setTo(cv::Scalar(0, 0, 255));
 
-	mask = Mat::zeros(image.size(), CV_8UC3);
-	finalMask = Mat::zeros(image.size(), CV_8UC3);
-	rectangle(mask, Point(0, mask.rows / 2), Point(mask.cols, mask.rows), Scalar(255, 255, 255), CV_FILLED);
-	cvtColor(mask, mask, CV_BGR2GRAY);
-
-
-	//convert grey
-	cvtColor(image, image_gray, CV_BGR2GRAY);
-
-	image_gray.copyTo(divided, mask);
-
-	GaussianBlur(divided, divided, Size(3, 3), 0, 0);
-
-	for (int i = 0;i < divided.cols;i++)
-	{
-		for (int j = 0;j < divided.rows;j++)
-		{
-			if (divided.at<uchar>(j, i) >= 200)
-				divided.at<uchar>(j, i) = 255;
-		}
-	}
-
-	threshold(divided, thresh, 150, max_BINARY_value, 0);
-
-	//morphological closing (fill small holes in the foreground)
-	dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-	erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-	//morphological closing (fill small holes in the foreground)
-	dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-	erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-	namedWindow("threshi", WINDOW_KEEPRATIO);
-	imshow("threshi", thresh);
-
-	//Canny
-	Canny(thresh, thresh, 15, 45, 3);
-
-	vector<Vec4i> lines;
-	HoughLinesP(thresh, lines, 1, CV_PI / 180, 15, 2, 10);
+mask = Mat::zeros(image.size(), CV_8UC3);
+finalMask = Mat::zeros(image.size(), CV_8UC3);
+rectangle(mask, Point(0, mask.rows / 2), Point(mask.cols, mask.rows), Scalar(255, 255, 255), CV_FILLED);
+cvtColor(mask, mask, CV_BGR2GRAY);
 
 
-	// draw lines
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		Vec4i l = lines[i];
-		double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
-		angle = angle < 0 ? angle + 360 : angle;
-		if ((angle > 20 && angle < 80) || (angle > 100 && angle < 160) || (angle > 200 && angle < 260) || (angle > 280 && angle < 340)) {
-			//line(finalMask, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 255, 255), 1, CV_AA);
-			line(thresh, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 1, CV_AA);
-		}
-	}
+//convert grey
+cvtColor(image, image_gray, CV_BGR2GRAY);
 
-	namedWindow("threshi1", WINDOW_KEEPRATIO);
-	imshow("threshi1", thresh);
+image_gray.copyTo(divided, mask);
 
-	thresh.copyTo(finalMask);
-	//cvtColor(finalMask, finalMask, CV_BGR2GRAY);
+GaussianBlur(divided, divided, Size(3, 3), 0, 0);
 
-	//draw between lines
-	bool foundRight = false, foundLeft = false;
-	int init, end;
+for (int i = 0;i < divided.cols;i++)
+{
+for (int j = 0;j < divided.rows;j++)
+{
+if (divided.at<uchar>(j, i) >= 200)
+divided.at<uchar>(j, i) = 255;
+}
+}
 
-	for (int i = 0;i < finalMask.rows;i++)
-	{
-		//find first right point
-		for (int j = finalMask.cols / 2;j < finalMask.cols;j++)
-		{
+threshold(divided, thresh, 150, max_BINARY_value, 0);
 
-			if (finalMask.at<uchar>(i, j) > 200)
-			{
-				end = j;
-				foundRight = true;
-				break;
-			}
+//morphological closing (fill small holes in the foreground)
+dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-		}
+//morphological closing (fill small holes in the foreground)
+dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-		for (int j = 0;j < finalMask.cols / 2;j++)
-		{
-			if (finalMask.at<uchar>(i, j) > 200)
-			{
-				init = j;
-				foundLeft = true;
-				break;
-			}
+namedWindow("threshi", WINDOW_KEEPRATIO);
+imshow("threshi", thresh);
 
-		}
+//Canny
+Canny(thresh, thresh, 15, 45, 3);
 
-		if (foundRight&&foundLeft)
-		{
-			for (int j = init;j <= end;j++)
-			{
-				finalMask.at<uchar>(i, j) = 255;
-			}
-		}
-		foundRight=false;
-		foundLeft = false;
-	}
+vector<Vec4i> lines;
+HoughLinesP(thresh, lines, 1, CV_PI / 180, 15, 2, 10);
 
-	image.copyTo(image_final);
-	blue.copyTo(image_final,finalMask);
 
-	namedWindow("thresh", WINDOW_KEEPRATIO);
-	imshow("thresh", thresh);
+// draw lines
+for (size_t i = 0; i < lines.size(); i++)
+{
+Vec4i l = lines[i];
+double angle = atan2(l[3] - l[1], l[2] - l[0]) * 180.0 / CV_PI;
+angle = angle < 0 ? angle + 360 : angle;
+if ((angle > 20 && angle < 80) || (angle > 100 && angle < 160) || (angle > 200 && angle < 260) || (angle > 280 && angle < 340)) {
+//line(finalMask, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 255, 255), 1, CV_AA);
+line(thresh, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 1, CV_AA);
+}
+}
 
-	namedWindow("test", WINDOW_KEEPRATIO);
-	imshow("test", image_final);
-	waitKey(0);
+namedWindow("threshi1", WINDOW_KEEPRATIO);
+imshow("threshi1", thresh);
+
+thresh.copyTo(finalMask);
+//cvtColor(finalMask, finalMask, CV_BGR2GRAY);
+
+//draw between lines
+bool foundRight = false, foundLeft = false;
+int init, end;
+
+for (int i = 0;i < finalMask.rows;i++)
+{
+//find first right point
+for (int j = finalMask.cols / 2;j < finalMask.cols;j++)
+{
+
+if (finalMask.at<uchar>(i, j) > 200)
+{
+end = j;
+foundRight = true;
+break;
+}
+
+}
+
+for (int j = 0;j < finalMask.cols / 2;j++)
+{
+if (finalMask.at<uchar>(i, j) > 200)
+{
+init = j;
+foundLeft = true;
+break;
+}
+
+}
+
+if (foundRight&&foundLeft)
+{
+for (int j = init;j <= end;j++)
+{
+finalMask.at<uchar>(i, j) = 255;
+}
+}
+foundRight=false;
+foundLeft = false;
+}
+
+image.copyTo(image_final);
+blue.copyTo(image_final,finalMask);
+
+namedWindow("thresh", WINDOW_KEEPRATIO);
+imshow("thresh", thresh);
+
+namedWindow("test", WINDOW_KEEPRATIO);
+imshow("test", image_final);
+waitKey(0);
 }*/
 
 
